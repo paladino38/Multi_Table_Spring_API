c24a7af3f256b60e232edc60c93f02176cfe007b
marco <marcoasp87@gmail.com>
2024-11-23 18:41:23 -0300

    Creating Services finished

--- src/main/java/org/gm2/pdv/loombok_tst/controller/SaleController.java
+++ src/main/java/org/gm2/pdv/loombok_tst/controller/SaleController.java
@@ -1,9 +1,41 @@
 package org.gm2.pdv.loombok_tst.controller;
 
+import org.gm2.pdv.loombok_tst.dto.SaleDTO;
+import org.gm2.pdv.loombok_tst.entity.User;
+import org.gm2.pdv.loombok_tst.repository.UserRepository;
+import org.gm2.pdv.loombok_tst.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/sale")
 public class SaleController {
+
+    private SaleService saleService;
+    private UserRepository userRepository;
+    public SaleController(@Autowired SaleService saleService) {
+        this.saleService = saleService;
+    }
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody SaleDTO saleDTO) {
+        try{
+
+
+           System.out.println("Valor do Userid é" + saleDTO.getUserId());
+            System.out.println("Valor do product é" + saleDTO.getItems());
+            long id = saleService.save(saleDTO);
+            return new ResponseEntity("Venda realisada com sucesso" + id, HttpStatus.CREATED);
+
+        }catch (Exception e) {
+                    return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/org/gm2/pdv/loombok_tst/service/SaleService.java
+++ src/main/java/org/gm2/pdv/loombok_tst/service/SaleService.java
@@ -1,5 +1,7 @@
 package org.gm2.pdv.loombok_tst.service;
 
+import jakarta.transaction.Transactional;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.gm2.pdv.loombok_tst.dto.SaleDTO;
 import org.gm2.pdv.loombok_tst.entity.Product;
@@ -7,6 +9,9 @@ import org.gm2.pdv.loombok_tst.entity.ItemSale;
 import org.gm2.pdv.loombok_tst.dto.ProductDTO;
 import org.gm2.pdv.loombok_tst.entity.Sale;
 import org.gm2.pdv.loombok_tst.entity.User;
+import org.gm2.pdv.loombok_tst.repository.ItemSaleRepository;
+import org.gm2.pdv.loombok_tst.repository.ProductRepository;
+import org.gm2.pdv.loombok_tst.repository.SaleRepository;
 import org.gm2.pdv.loombok_tst.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,23 +19,50 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Data
 public class SaleService {
 
+    private final SaleRepository saleRepository;
     private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final ItemSaleRepository itemSaleRepository;
 
-
+    @Transactional
     public long save(SaleDTO sale){
         User user = userRepository.findById(sale.getUserId()).get();
         Sale newSale = new Sale();
         newSale.setUser(user);
-        newSale.s
         newSale.setDate(LocalDateTime.now());
+        List<ItemSale> items = getItemSale(sale.getItems());
+
+        newSale = saleRepository.save(newSale);
+
+        saveItemSale(items, newSale);
+
+        return newSale.getId();
     }
 
-    private List<ItemSale> getItemSale(List<ProductDTO> products>){
+    private void saveItemSale(List<ItemSale> items, Sale newSale) {
+        for (ItemSale item : items) {
+            item.setSale(newSale);
+            itemSaleRepository.save(item);
+        }
+    }
+
+    private List<ItemSale> getItemSale(List<ProductDTO> products){
+        return products.stream().map(item ->{
+            Product product = productRepository.getReferenceById(item.getProductId());
+
+            ItemSale itemSale = new ItemSale();
+            itemSale.setProduct(product);
+            itemSale.setQuantity(item.getQuantity());
+            return itemSale;
+        }).collect(Collectors.toList());
 
     }
 }
